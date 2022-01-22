b5ddbb080d91c1776a972784256150aa7613b1ff
fsidvpn <97771305+fsidvpn@users.noreply.github.com>
2022-01-22 22:24:23 +0700

    Update addssh.sh

--- ssh/addssh.sh
+++ ssh/addssh.sh
@@ -42,8 +42,8 @@ sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
 ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 clear
-systemctl restart ws-tls
-systemctl restart ws-nontls
+systemctl restart ws-ovpn
+systemctl restart websocket
 systemctl restart ssh-ohp
 systemctl restart dropbear-ohp
 systemctl restart openvpn-ohp
