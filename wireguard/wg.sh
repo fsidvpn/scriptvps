db240045cf10261c4ae983f9fa6376bd4ffb0691
fsidvpn <97771305+fsidvpn@users.noreply.github.com>
2022-02-22 09:19:12 +0700

    Update wg.sh

--- wireguard/wg.sh
+++ wireguard/wg.sh
@@ -49,7 +49,7 @@ if [[ -e /etc/wireguard/params ]]; then
 	exit 1
 fi
 
-echo -e "${Info} Wireguard Script By Akbar Maulana"
+echo -e "${Info} Wireguard Script By FSID VPN"
 # Detect public IPv4 address and pre-fill for the user
 
 # Detect public interface and pre-fill for the user
