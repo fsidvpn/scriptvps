89fc3121a97232aa96cc38e743455ea368b24215
fsidvpn <97771305+fsidvpn@users.noreply.github.com>
2022-02-22 09:51:53 +0700

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -31,7 +31,7 @@ DOMAIN=fsind.site
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
 SUB_DOMAIN=${sub}.fsind.site
 CF_ID=awaledyan@gmail.com
-CF_KEY=0274dfd4c74a369ce7f28c048be8ca11f7828
+CF_KEY=7c9a592ee3186932780cffb3f1b6dbfee8084
 set -euo pipefail
 IP=$(wget -qO- ipinfo.io/ip);
 echo "Updating DNS for ${SUB_DOMAIN}..."
